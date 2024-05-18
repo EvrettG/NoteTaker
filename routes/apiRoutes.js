94bd7936ee92a6129ed96be5d8504987158555cf
EvrettG <skyrift@tpg.com.au>
2024-05-18 22:21:08 +1000

    Can now read and load notes, although currently only showing titles and not text

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,19 @@
-
+// used to handle fetch requests from index.js
 const api = require('express').Router();
+// Where all files are stored
 const noteDB = require('../db/db.json');
 
+const {readFromFile, writeToFile, readAndAppend,} = require("../helpers/fsUtils");
 
-
-
-api.get('/api/notes', (req, res)=>{
+// appon reciving a get request for nots (or actually /api/notes) this function then reads from the db.json
+// and returns it as usable string
+api.get('/notes', (req, res)=>{
     console.log(`${req.method} request received to get notes`);
-    return res.json(noteDB)
+    // then converts it into usable js objuct for use
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-api.post('/api/notes', (req, res)=>{
+api.post('/notes', (req, res)=>{
     console.log(`${req.method} request received to post to notes`);
     // prepare a response object to send data back to a client
     let response
@@ -23,5 +26,9 @@ api.post('/api/notes', (req, res)=>{
     }
     console.log(req.bodey)
 });
+
+api.delete('/notes', (req, res)=>{
+
+})
 
 module.exports = api
