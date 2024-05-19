2f7cafc368d4a5ad1bcde21b06005e10da4a8620
EvrettG <skyrift@tpg.com.au>
2024-05-19 21:37:17 +1000

    Delete function now works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,15 +11,14 @@ api.get('/notes', (req, res)=>{
     // then converts it into usable js objuct for use
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
-
+// this function add's new note's to the db.json
 api.post('/notes', (req, res)=>{
     console.log(`${req.method} request received to post to notes`);
     // Destructures the 
     const {title, text } = req.body
-    
-
     // check if post request has at least title and text
     if(req.body?.title && req.body?.text) {
+        // newNote is created from 
         const newNote = {
             title,
             text,
@@ -34,11 +33,26 @@ api.post('/notes', (req, res)=>{
     } else{
         res.json(`request has been denined must contain title and text`)
     }
-
 });
+// this performs the delete funtion when a delete request is sent
+// :id should register anything after notes
+api.delete(`/notes/:id`, (req, res)=>{
+    // this takes the :id part of the searchbar parameters and then turns it into a variable
+    const noteID = req.params.id;
+    // reads from the db.json 
+    readFromFile('./db/db.json')
+    // after that happens the next functions turns it from string to json
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            // this function should then run a filter removing any object with the noteID from the db.json
+            const newDB = json.filter((note) => note.id !== noteID);
+            writeToFile('./db/db.json', newDB);
 
-api.delete('/notes', (req, res)=>{
-
+            // NOTE: if noteID is writen as noteId or anything else the proram crashes and kills the database
+            res.json(`Note ${noteID} has been deleted`);
+        })
+    // console.log(`${req.method} request received to post to notes`);
+    // oldNotes = readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 module.exports = api
